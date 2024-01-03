2eef5658da92a4ed2ca991829fb8b4e6409d19a0
Lorentz Lasson <lasson.lorentz@gmail.com>
2024-01-03 19:57:56 +0100

    Simplify upgrade script
    Align with debian docker file
    https://github.com/roc-lang/roc/blob/main/docker/nightly-debian-latest/Dockerfile

--- upgrade_roc.sh
+++ upgrade_roc.sh
@@ -1,7 +1,3 @@
-curl -OL https://github.com/roc-lang/roc/releases/download/nightly/roc_nightly-linux_x86_64-latest.tar.gz
-mkdir rocextract
-tar -xf roc_nightly-linux_x86_64-latest.tar.gz -C rocextract
-rocdir=$(ls rocextract)
-mv ./rocextract/$rocdir/roc ~/.local/bin/roc
-
-rm -rf roc_nightly-linux_x86_64-latest.tar.gz rocextract
+wget -q -O roc.tar.gz https://github.com/roc-lang/roc/releases/download/nightly/roc_nightly-linux_x86_64-latest.tar.gz
+tar -xvz -f roc.tar.gz --directory ~/.local/bin --strip-components=1
+rm roc.tar.gz
